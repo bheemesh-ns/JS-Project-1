ef12733983ce6f06939d4350ecfadc516c9c947a
Bheemesh N S <bheeman902@gmail.com>
2025-11-27 15:14:39 +0530

    final repo

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function sub(){
 
     document.getElementById('output').innerText = Number(num1.value) - Number(num2.value);
 }
+
+function mul(){
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+
+    document.getElementById('output').innerText = Number(num1.value) * Number(num2.value);
+}
+
+function div(){
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    
+    document.getElementById('output').innerText = Number(num1.value) / Number(num2.value);
+}
